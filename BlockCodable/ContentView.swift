09c5223995d3922537c33fc7ac4d83b46db7dab4
유인호 <yuiop1029@naver.com>
2024-12-09 22:01:59 +0900

    init

--- BlockCodable/ContentView.swift
+++ BlockCodable/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var testModel = TestModel(value: "Hello", name: "World")
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(
+                data: try! JSONEncoder().encode(testModel),
+                encoding: .utf8
+            ) ?? "")
         }
         .padding()
     }
@@ -21,4 +22,30 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct TestModel: Codable {
+    @BlockCodable var value: String?
+    var name: String
+}
+
+
+@propertyWrapper
+struct BlockCodable<T: Codable> where T: ExpressibleByNilLiteral {
+    var wrappedValue: T
+
+    init(wrappedValue: T) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+extension BlockCodable: Codable {
+    init(from decoder: Decoder) throws {
+        self.wrappedValue = nil
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
 }
