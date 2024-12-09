66d1b031b4d8cd4b5e2557ebb37ebb8120c81f9c
유인호 <yuiop1029@naver.com>
2024-12-09 21:45:42 +0900

    Initial Commit

---
+++ BlockCodable/BlockCodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockCodableApp.swift
+//  BlockCodable
+//
+//  Created by A_Mcflurry on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BlockCodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
